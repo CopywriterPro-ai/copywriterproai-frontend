3a35bfc67c49db4ff23a328bc66b19b6cd89fc90
nafisfaysal <mirzanafisfaysal@gmail.com>
2024-06-09 12:39:12 +0200

    add section api key

--- components/hero-section/HeroSection2.js
+++ components/hero-section/HeroSection2.js
@@ -1,8 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useState, useEffect, useMemo } from "react";
-import Link from "next/link";
-import { IoPlayCircleOutline } from "react-icons/io5";
-import { FiLifeBuoy, FiGithub, FiFigma } from "react-icons/fi";
+import React, { useState, useEffect } from "react";
+import { FiLifeBuoy, FiGithub, FiFigma, FiCheckCircle } from "react-icons/fi";
 import ModalVideo from "react-modal-video";
 import styled from "styled-components";
 import classNames from "classnames";
@@ -10,7 +8,7 @@ import classNames from "classnames";
 import DemoBlogHeadline from "@/components/pages/landing/Banner/components/DemoBlogHeadline";
 import DemoParaphrase from "@/components/pages/landing/Banner/components/DemoParaphrase";
 
-import { GoogleButton, FacebookButton } from "@/components/common/AuthButton";
+import { GoogleButton } from "@/components/common/AuthButton";
 import { isServer } from "@/utils";
 
 const baseURL = process.env.NEXT_PUBLIC_APP_API_URL;
@@ -31,9 +29,24 @@ const features = [
   },
   {
     id: "ai-powered",
-    title: "Al-Powered: High-Quality Content Tailored to You",
+    title: "AI-Powered: High-Quality Content Tailored to You",
   },
   { id: "seo-friendly", title: "SEO-Friendly: Boost Your Online Visibility" },
+];
+
+const apiFeatures = [
+  {
+    id: "seamless-integration",
+    title: "Seamless Integration with Your Existing APIs",
+  },
+  {
+    id: "no-extra-charges",
+    title: "No Extra Charges for API Usage",
+  },
+  {
+    id: "complete-control",
+    title: "Complete Control Over Your Content Generation",
+  },
 ];
 
 const handleStrategyAction = (strategy) => {
@@ -45,7 +58,7 @@ const SliderContent = [
   { Slider: DemoBlogHeadline },
 ];
 
-const HeroSectionOne = () => {
+const HeroSectionTwo = () => {
   const [activeSlide, setActiveSlide] = useState(0);
   const [currentSlide, setCurrentSlide] = useState({
     Slider: SliderContent[0].Slider,
@@ -65,155 +78,127 @@ const HeroSectionOne = () => {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <section
-      className="hero-section ptb-120 text-white bg-gradient"
-      style={{ background: "url('/hero-dot-bg.png')no-repeat center right" }}
-    >
-      <ModalVideo
-        channel="youtube"
-        isOpen={isOpen}
-        videoId="aQFao8lz6C8"
-        onClose={() => setOpen(false)}
-      />
-      <div className="container">
-        <div className="row align-items-center mt-40">
-          <div className="col-lg-6 col-md-10">
-            <div className="hero-content-wrap mt-100 mt-lg-0 mt-xl-0">
-              <HeadingStyle>
-                Open-Source Al Writing Platform for SEO and Ad Copy
-              </HeadingStyle>
-              <DescriptionStyle>
-                CopywriterPro is the world&apos;s first open-source Al content
-                writing platform that empowers users to create SEO-friendly blog
-                posts, ad copy for social media, website landing pages, and
-                more. With its advanced Al algorithms and versatile content
-                creation capabilities, CopywriterPro offers unparalleled
-                flexibility and freedom.
-              </DescriptionStyle>
+      <>
+        <section
+            className="hero-section ptb-120 text-white bg-gradient"
+            style={{ background: "url('/hero-dot-bg.png')no-repeat center right" }}
+        >
+          <ModalVideo
+              channel="youtube"
+              isOpen={isOpen}
+              videoId="aQFao8lz6C8"
+              onClose={() => setOpen(false)}
+          />
+          <div className="container">
+            <div className="row align-items-center mt-40">
+              <div className="col-lg-6 col-md-10">
+                <div className="hero-content-wrap mt-100 mt-lg-0 mt-xl-0">
+                  <HeadingStyle>
+                    Open-Source AI Writing Platform for SEO and Ad Copy
+                  </HeadingStyle>
+                  <DescriptionStyle>
+                    CopywriterPro is the world&apos;s first open-source AI content
+                    writing platform that empowers users to create SEO-friendly
+                    blog posts, ad copy for social media, website landing pages,
+                    and more. With its advanced AI algorithms and versatile
+                    content creation capabilities, CopywriterPro offers
+                    unparalleled flexibility and freedom.
+                  </DescriptionStyle>
 
-              <FeaturesStyle>
-                {features.map((feature) => (
-                  <FeatureStyle key={feature.id}>
-                    <FiLifeBuoy /> {feature.title}
-                  </FeatureStyle>
-                ))}
-              </FeaturesStyle>
+                  <FeaturesStyle>
+                    {features.map((feature) => (
+                        <FeatureStyle key={feature.id}>
+                          <FiLifeBuoy /> {feature.title}
+                        </FeatureStyle>
+                    ))}
+                  </FeaturesStyle>
 
-              <div className="action-btns mt-5 align-items-center flex d-sm-flex d-lg-flex d-md-flex">
-                <div style={{ display: "flex", alignItems: "center", gap: 20 }}>
-                  <ActionBtn>
-                    <FiGithub />
-                    View on Github
-                  </ActionBtn>
-                  <ActionBtn onClick={() => handleStrategyAction("google")}>
-                    <FiFigma />
-                    Try for free
-                  </ActionBtn>
+                  <div className="action-btns mt-5 align-items-center flex d-sm-flex d-lg-flex d-md-flex">
+                    <div style={{ display: "flex", alignItems: "center", gap: 20 }}>
+                      <ActionBtn>
+                        <FiGithub />
+                        View on Github
+                      </ActionBtn>
+                      <ActionBtn onClick={() => handleStrategyAction("google")}>
+                        <FiFigma />
+                        Try for free
+                      </ActionBtn>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6 col-md-12 mt-4 mt-lg-0">
+                <img
+                    width="100%"
+                    src="https://plus.unsplash.com/premium_photo-1661877737564-3dfd7282efcb"
+                    alt="Screenshot"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="container" style={{ marginTop: "50px" }}>
+            <div
+                style={{
+                  display: "flex",
+                  overflow: "hidden",
+                  justifyContent: "space-around",
+                }}
+            >
+              {brands.map((brand) => (
+                  <div key={brand.name} title={brand.name}>
+                    <img width="180px" src="/logo-color.svg" alt={brand.name} />
+                  </div>
+              ))}
+            </div>
+          </div>
+        </section>
+        <section className="api-section ptb-120 bg-light text-dark">
+          <div className="container">
+            <div className="row align-items-center mt-40 flex-column-reverse flex-lg-row">
+              <div className="col-lg-6 col-md-12 mt-4 mt-lg-0">
+                <img
+                    width="100%"
+                    src="https://plus.unsplash.com/premium_photo-1661877737564-3dfd7282efcb"
+                    alt="Onboard Screenshot"
+                />
+              </div>
+              <div className="col-lg-6 col-md-12">
+                <div className="api-content-wrap mt-100 mt-lg-0 mt-xl-0">
+                  <HeadingStyle>Use Your Own API Keys for Free</HeadingStyle>
+                  <DescriptionStyle>
+                    With CopywriterPro, you have the option to use your own API keys
+                    for free. This means you can take advantage of all our powerful
+                    features without any additional cost.
+                  </DescriptionStyle>
+
+                  <FeaturesStyle>
+                    {apiFeatures.map((feature) => (
+                        <FeatureStyle key={feature.id}>
+                          <FiCheckCircle /> {feature.title}
+                        </FeatureStyle>
+                    ))}
+                  </FeaturesStyle>
+
+                  <div className="action-btns mt-5 d-sm-flex d-lg-flex d-md-flex">
+                    <div style={{ display: "flex", alignItems: "center", gap: 20 }}>
+                      <ActionBtn>
+                        <FiCheckCircle />
+                        Add Your API Key
+                      </ActionBtn>
+                      <ActionBtn>
+                        <FiCheckCircle />
+                        Try for free
+                      </ActionBtn>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-lg-6 col-md-12 ">
-            <img
-              width="100%"
-              src="https://plus.unsplash.com/premium_photo-1661877737564-3dfd7282efcb"
-              alt=""
-            />
-          </div>
-        </div>
-      </div>
-      <div className="container" style={{ marginTop: "50px" }}>
-        <div
-          style={{
-            display: "flex",
-            overflow: "hidden",
-            justifyContent: "space-around",
-          }}
-        >
-          {brands.map((brand) => (
-            <div key={brand.name} title={brand.name}>
-              <img width="180px" src="/logo-color.svg" alt={brand.name} />
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
+        </section>
+      </>
   );
 };
-
-const Demo = styled.div`
-  @media screen and (max-width: 1060px) {
-    margin-top: 12rem;
-    width: 80%;
-    display: flex;
-    justify-content: center;
-  }
-`;
-
-const DemoContainer = styled.div`
-  poition: relative;
-`;
-
-const DemoContent = styled.div`
-  position: relative;
-  display: flex;
-
-  @media screen and (max-width: 1060px) {
-    flex-direction: column;
-  }
-`;
-
-const SliderControl = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  padding: 5px 0;
-  margin-left: 1rem;
-
-  @media screen and (max-width: 1060px) {
-    margin-left: 0;
-    margin-top: 0.5rem;
-    justify-content: center;
-  }
-
-  @media (max-width: 768px) {
-    justify-content: center;
-  }
-`;
-
-const SliderDot = styled.ul`
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
-  gap: 5px;
-
-  li {
-    margin: 5px;
-
-    button {
-      background: white;
-      border-radius: 50%;
-      border: none;
-      cursor: pointer;
-      height: 20px;
-      outline: none;
-      padding: 5px;
-      width: 20px;
-    }
-
-    .active {
-      background: #0096f6;
-    }
-  }
-
-  @media screen and (max-width: 1060px) {
-    display: flex;
-
-    li {
-      margin: 3px;
-    }
-  }
-`;
 
 const HeadingStyle = styled.h1`
   margin-bottom: 15px;
@@ -244,4 +229,4 @@ const ActionBtn = styled.div`
   box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
 `;
 
-export default HeroSectionOne;
+export default HeroSectionTwo;
